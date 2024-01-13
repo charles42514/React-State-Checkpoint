7546eed97b1aaab775a476eb3883b0e711996be7
charles42514 <charlesdavid4251@gmail.com>
2024-01-14 00:49:35 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      person: {
+        fullName: 'Charles Okpara',
+        bio: 'I am a university student learning coding at GoMyCode',
+        imgSrc: 'https://media-los2-1.cdn.whatsapp.net/v/t61.24694-24/349968366_707033508139832_5036399013722424177_n.jpg?ccb=11-4&oh=01_AdRsxM5vrmPqQJanqtUVd52VmQeZuUSije7OqIYlYcBJNg&oe=65B03248&_nc_sid=e6ed6c&_nc_cat=103',
+        profession: 'Frontend Developer',
+      },
+      show: false,
+      mountTime: new Date(),
+    };
+  }
+
+  componentDidMount() {
+    this.intervalId = setInterval(this.updateMountTime, 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.intervalId);
+  }
+
+  updateMountTime = () => {
+    this.setState({ mountTime: new Date() });
+  };
+
+  toggleShow = () => {
+    this.setState((prevState) => ({ show: !prevState.show }));
+  };
+
+  render() {
+    const { person, show, mountTime } = this.state;
+
+    return (
+      <div>
+        <h1>React State App</h1>
+        <button onClick={this.toggleShow}>Toggle Profile</button>
+
+        {show && (
+          <div>
+            <h2>{person.fullName}</h2>
+            <p>{person.bio}</p>
+            <img src={person.imgSrc} alt={person.fullName} />
+            <p>Profession: {person.profession}</p>
+          </div>
+        )}
+
+        <p>Component Mount Time: {mountTime.toLocaleTimeString()}</p>
+      </div>
+    );
+  }
 }
 
 export default App;
